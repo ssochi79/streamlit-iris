9d108869fb5a52b8ebe7c1daacaba7f2c9b5b462
ssochi79 <112685588+ssochi79@users.noreply.github.com>
2025-06-04 18:28:36 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -2,18 +2,21 @@ import streamlit as st
 import numpy as np
 import joblib
 
+# 모델 불러오기
 model = joblib.load("model.pkl")
 
 st.title("꽃 분류기 (Iris Classifier)")
 st.write("입력값을 기반으로 꽃의 종류를 예측합니다.")
 
-sepal_length = st.slider("Sepal Length (cm)",4.0, 8.0, 5.1)
-sepal_width = st.slider("Sepal Width (cm)",2.0, 4.5, 3.5)
+sepal_length = st.slider("Sepal Length (cm)", 4.0, 8.0, 5.1)
+sepal_width = st.slider("Sepal Width (cm)", 2.0, 4.5, 3.5)
 petal_length = st.slider("Petal Length (cm)", 1.0, 7.0, 1.4)
 petal_width = st.slider("Petal Width (cm)", 0.1, 2.5, 0.2)
 
-inpurt_data = np.array([[sepal_length, sepal_width, petal_length, petal_width]])
+input_data = np.array([[sepal_length, sepal_width, petal_length, petal_width]])
 
-prediction = model.preditc(input_data)
+prediction = model.predict(input_data)
 predicted_class = prediction[0]
-class_names = ['Setosa', ' Versicolor', 'Virginica']
+class_names = ['Setosa', 'Versicolor', 'Virginica']
+
+st.write(f"예측 결과: **{class_names[predicted_class]}**")
--- train_model.py
+++ train_model.py
@@ -2,8 +2,9 @@ from sklearn.datasets import load_iris
 from sklearn.ensemble import RandomForestClassifier
 import joblib
 
-X,y = load_iris(retrun_X_y=True)
-model = RandomForestClssifier()
-model.fit(X,y)
+X, y = load_iris(return_X_y=True)
+model = RandomForestClassifier()
+model.fit(X, y)
 
 joblib.dump(model, 'model.pkl')
+print("model.pkl 저장 완료")
